9623ee5981e02e2479530bc3e9ac1b92a6803922
MuRye <dreamaker7770@gmail.com>
2017-04-10 11:58:26 +0900

    Base structure

--- src/dimibob.js
+++ src/dimibob.js
@@ -1,3 +1,17 @@
-export default class {
-  
+const uri = 'http://dimigo.in/pages/dimibob_getdata.php?d='
+
+class Bob {
+  getToday () {
+    // TODO
+  }
+
+  getYesterday () {
+    // TODO
+  }
+
+  _request (day) {
+    // TODO
+  }
 }
+
+export default new Bob(uri)
