d06778cd0484d2b4f67c85ad1c000ca0a30114e8
Chalk <chalkpe@gmail.com>
2018-04-05 10:30:18 +0900

    refactor pull request

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 const dimibob = require('../lib')
+const { string: fmt } = require('../lib/fmt')
 
 async function bob (date = new Date()) {
   const { breakfast, lunch, dinner, snack } = await dimibob(date)
   const [y, m, d] = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
-  const fmt = str => (str || 'X').split('\n').join(', ')
 
   console.log(`
   오늘도 맛있는 아라코 ^^
--- test/index.js
+++ test/index.js
@@ -1,10 +1,14 @@
 const dimibob = require('../lib')
+const { string: fmt } = require('../lib/fmt')
 
-;(async () => {
-  const fmt = str => (str || 'X').split('\n').join(', ')
-  const show = ({breakfast, lunch, dinner, snack}) => `아침: ${fmt(breakfast)}\n점심: ${fmt(lunch)}\n저녁: ${fmt(dinner)}\n간식: ${fmt(snack)}\n`
-  const now = new Date()
+const show = ({ breakfast, lunch, dinner, snack }) =>
+  `아침: ${fmt(breakfast)}\n점심: ${fmt(lunch)}\n저녁: ${fmt(dinner)}\n간식: ${fmt(snack)}\n`
 
-  console.log('Daily Meal:\n' + show(await dimibob.daily(now)))
-  console.log('Monthly Meals:\n' + (await dimibob.monthly(now)).map(show).join('\n'))
-})()
+async function test (now = new Date()) {
+  console.log('Daily:\n' + show(await dimibob.daily(now)))
+  console.log('Monthly:\n' + (await dimibob.monthly(now)).map(show).join('\n'))
+}
+
+test()
+  .then(() => console.log('Done.') || process.exit(0))
+  .catch(err => console.error(err) || process.exit(1))
